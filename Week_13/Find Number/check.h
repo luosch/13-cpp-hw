a55784ef67936f63338600d020c2f8cdab359346
luosch <luosch@mail2.sysu.edu.cn>
2014-05-21 12:26:02 +0800

    my cpp hw

--- Week_13/Find Number/check.h
+++ Week_13/Find Number/check.h
@@ -1,3 +1,7 @@
+// num[][]是矩阵,row是行数和列数(相等的),tar是目标值
+
+// Step-wise线性搜索
+// 时间复杂度O(n)
 #define valid(x, y, s) (x >= 0 && y >= 0 && x < s && y < s)
 bool check(int num[][1000], int row, const int &tar) {
     int X = row-1, Y = 0;
@@ -8,3 +12,26 @@ bool check(int num[][1000], int row, const int &tar) {
     }
     return 0;
 }
+
+// 二分查找大法
+// 时间复杂度O(nlog(n))
+// bool binarySearch(int num[][1000], int i, const int &size, const int &tar) {
+    // int l = 0, r = size-1, mid = (l+r)>>1;
+    // while (l < r) {
+        // if (num[i][mid] < tar) {
+            // l = mid+1;
+        // }
+        // if (num[i][mid] > tar) {
+            // r = mid;
+        // }
+        // mid = (l+r)>>1;
+        // if (num[i][mid] == tar) return 1;
+    // }
+    // return 0;
+// }
+// bool check(int num[][1000], int row, const int &tar) {
+    // for (int i = 0; i < row; i++) {
+       // if (binarySearch(num, i, row, tar)) return 1;
+    // }
+    // return 0;
+// }
